5d2269f7068a71e56315a58b83a054ac70ee3dae
yash <yashkumar201301@gmail.com>
2021-01-02 00:17:26 +0530

    initial commit

--- app/src/androidTest/java/com/yash/mvvmtodo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yash/mvvmtodo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.codinginflow.mvvmtodo
+package com.yash.mvvmtodo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/com/yash/mvvmtodo/MainActivity.kt
+++ app/src/main/java/com/yash/mvvmtodo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.codinginflow.mvvmtodo
+package com.yash.mvvmtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
